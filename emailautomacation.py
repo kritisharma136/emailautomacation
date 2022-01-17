80d1941d5f5db1e270695cf2d31658551228aaee
Kriti <kritisharmaa136@gmail.com>
2022-01-17 23:20:45 +0530

    Initial commit

---
+++ emailautomacation.py
@@ -0,0 +1,40 @@
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+
+def send_email(request):
+    subject = request.POST.get('subject', '')
+    message = request.POST.get('message', '')
+    from_email = request.POST.get('from_email', '')
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, ['admin@example.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/thanks/')
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.
+        return HttpResponse('Make sure all fields are entered and valid.')
+    
+from django.core.mail import EmailMultiAlternatives
+
+subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
+text_content = 'This is an important message.'
+html_content = '<p>This is an <strong>important</strong> message.</p>'
+msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+msg.attach_alternative(html_content, "text/html")
+msg.send()
+
+from django.core import mail
+
+with mail.get_connection() as connection:
+    mail.EmailMessage(
+        subject1, body1, from1, [to1],
+        connection=connection,
+    ).send()
+    mail.EmailMessage(
+        subject2, body2, from2, [to2],
+        connection=connection,
+    ).send()
+    
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
